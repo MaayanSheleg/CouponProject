3c57a164b54add3d8c86cc379be50bcc3abb13a5
maayanf14 <maayanf14@Maayan>
2019-08-30 11:10:49 +0300

    new update- login

---
+++ CouponProject/src/main/java/com/mbms/beans/User.java
@@ -0,0 +1,43 @@
+package com.mbms.beans;
+
+public class User {
+
+	private String name;
+	private String pass;
+	private String type;
+
+	public User() {
+	}
+	
+
+	public User(String name, String pass, String type) {
+		this.name = name;
+		this.pass = pass;
+		this.type = type;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return pass;
+	}
+
+	public void setPassword(String password) {
+		this.pass = password;
+	}
+
+	public String getClientType() {
+		return type;
+	}
+
+	public void setClientType(String clientType) {
+		this.type = clientType;
+	}
+}
